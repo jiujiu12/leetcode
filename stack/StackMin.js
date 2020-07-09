37accb8ed8563df79e4c40956ffe08c533a4269b
furry <18834176546@163.com>
2020-07-09 22:40:44 +0800

    取栈的最小值 第三种方法

--- stack/StackMin.js
+++ stack/StackMin.js
@@ -2,59 +2,88 @@
  * initialize your data structure here.
  */
 var MinStack = function() {
+    // top指针
     this.topIndex = -1;
+    
+    // 最大值栈
+    this.minStack = [];
+    // 要求栈
     this.arr = [];
 };
 
 /** 
+ * 入栈
  * @param {number} x
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
+    // top指针上移
     this.topIndex++;
-    this.arr[this.topIndex] = x;
+
+    // 将新的top指针赋给newTop变量
+    let newTop = this.topIndex;
+
+    // 将元素放到newTop指向的位置
+    this.arr[newTop] = x;
+
+    // 给最小值栈赋值
+    if(newTop === 0 || this.minStack[newTop-1] >= x){
+        this.minStack[newTop] = this.arr[newTop];
+    }else if(this.minStack[newTop-1] < x){
+         this.minStack[newTop] =  this.minStack[newTop-1];
+    }
+    // 返回新增的元素
     return x;
-    // console.log(this.arr[this.topIndex]);
 };
 
 /**
+ * 出栈
  * @return {void}
  */
 MinStack.prototype.pop = function() {
     let val = this.arr[this.topIndex];
     this.topIndex--;
     this.arr.length--;
+    this.minStack.length--;
+
+    // 返回删除元素值
     return val;
-    // console.log(this.topIndex);
 };
 
 /**
+ * 返回top元素
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    // console.log(this.arr[this.topIndex]);
     return this.arr[this.topIndex];
 };
 
 /**
+ * 获取最小值
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    // var min = null;
-    // for(var i = 0;i < this.arr.length;i++){
-    //     if(min === null){
-    //        min = this.arr[i];
-    //     }else{
-    //         if(this.arr[i] < min){
-    //             min = this.arr[i];
-    //         }
-    //     }
-    // }
-    var min = Math.min.apply(this,this.arr);
+    /* 笨办法：
+    var min = null;
+    for(var i = 0;i < this.arr.length;i++){
+        if(min === null){
+           min = this.arr[i];
+        }else{
+            if(this.arr[i] < min){
+                min = this.arr[i];
+            }
+        }
+    } */
 
-    return min;
+    //JS Math函数： var min = Math.min.apply(this,this.arr);（写法简便，但是较为耗时）
+
+    // 定义最小值栈获取最小值，只需要取最小值栈的顶部元素即可
+    
+    return this.minStack[this.topIndex];
+
 };
 
+// 测试用例
 var minStack = new MinStack();
 minStack.push(-2);
 minStack.push(0);
